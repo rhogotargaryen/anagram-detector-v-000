2877bc7afa24000dc18b8e69dc12c25debe51962
Derek Haller <dhaller.personal@gmail.com>
2018-01-30 21:15:17 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,7 +2,7 @@ class Anagram
   attr_accessor :word
   def match(w_array)
     w_array.each do |x|
-      if x.scan(/\w+/) { |match|  } == word.scan(/\w+/)
+      if x.scan(/\w+/) == word.scan(/\w+/)
         x
       end
     end
