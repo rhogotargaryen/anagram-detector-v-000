9c2c3c4ba6d154926750c452afd5066e7b300e49
Derek Haller <dhaller.personal@gmail.com>
2018-01-30 22:29:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+  def match(w_array)
+    if w_array.include?(@name)
+      return @name
+    else
+      w_array.select! { |x| x.length == @name.length }
+      t = []
+      a= []
+      v = @name.scan /\w/
+      w_array.each do |x|
+        r = x.scan /\w/
+        t << r
+      end
+      t.each do |x|
+        if x.sort == v.sort
+          a << x.join
+        end
+      end
+    end
+    a
+  end
+end
