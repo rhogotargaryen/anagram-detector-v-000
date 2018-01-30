b0759a7b583d47918ef6702045eae9d6878818cc
Derek Haller <dhaller.personal@gmail.com>
2018-01-30 22:10:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,14 +5,30 @@ class Anagram
   end
   def match(w_array)
     self.match?(w_array)
+    self.ana(w_array)
   end
   def match?(w_array)
-    w_array.include?(@name) ? @name : []
+    if w_array.include?(@name)
+      return @name
+    else
+      w_array
+    end
   end
-  def detect(w_array)
+  def parse(w_array)
+    w_array.select! { |x| x.length == @name.length }
+    w_array
+  end
+  def ana(w_array)
+    t = []
+    v = @name.scan /\w/
     w_array.each do |x|
-      if x.length != @name.length
-        w_array.remove(x)
+      r = x.scan /\w/
+      t << v
+    end
+    t.each do |x|
+      if x.sort == v.sort
+        return x.join
       end
     end
+  end
 end
