5fce6257a3218ac32abb487d5349f75861a979b6
Derek Haller <dhaller.personal@gmail.com>
2018-01-30 21:14:47 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def match(w_array)
+    w_array.each do |x|
+      if x.scan(/\w+/) { |match|  } == word.scan(/\w+/)
+        x
+      end
+    end        
+        
+  end
+end
