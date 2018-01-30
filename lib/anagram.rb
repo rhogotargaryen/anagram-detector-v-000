f9e393d04239eea6e7891675a9f3a451573ffd13
Derek Haller <dhaller.personal@gmail.com>
2018-01-30 21:27:03 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
   def match(w_array)
     w_array.include?(@name) ? @name : []
   end
-  def match(w_array)
+  def detector(w_array)
     w_array.each do |x|
       if x.scan(/\w+/) == @name.scan(/\w+/)
         x
