5c8a43d57799da2ae973a290966a484895591f01
Derek Haller <dhaller.personal@gmail.com>
2018-01-30 21:44:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,4 +9,10 @@ class Anagram
   def match?(w_array)
     w_array.include?(@name) ? @name : []
   end
+  def detect(w_array)
+    w_array.each do |x|
+      if x.length != @name.length
+        w_array.remove(x)
+      end
+    end
 end
